6c88de2b45d06201be39ad43cd36d2ecd29b58b4
Markiian <kekavezmarkiyan@gmail.com>
2023-02-17 16:05:58 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right_panel_active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right_panel_active");
+});
+
